8661ecbd0a020d23af14a00be7fbaa09d5551d19
Piotr Łepkowski <ozrabal@gmail.com>
2013-12-08 00:32:30 +0100

    Main file and dir structure

---
+++ pwp.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The WordPress Plugin PWP
+ *
+ * @package   PWP
+ * @author    Piotr Łepkowski <ozrabal@gmail.com>
+ * @license   GPL-2.0+
+ * @link      http://webkowski.com
+ * @copyright 2013 Piotr Łepkowski
+ *
+ * @wordpress-plugin
+ * Plugin Name:       PWP
+ * Plugin URI:        http://webkowski.com
+ * Description:       @TODO
+ * Version:           0.0.1
+ * Author:            Piotr Łepkowski
+ * Author URI:        http://webkowski.com
+ * Text Domain:       pwp
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/ozrabal/pwp
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
