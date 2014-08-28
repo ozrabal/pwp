681a3bc1febe31120bdfca69e16cc6027dabbafd
Piotr Łepkowski <ozrabal@gmail.com>
2014-08-28 15:45:02 +0200

    glowny plik pluginu
    usuniete niepotrzebne fragmenty kodu

--- pwp.php
+++ pwp.php
@@ -23,8 +23,6 @@
  */
 
  ob_start();
- //session_start();
-
 
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
@@ -35,11 +33,8 @@ define( 'PWP_ROOT', plugin_dir_path( __FILE__ ) );
 define( 'PWP_EXTERNAL_LIBRARY',  plugin_dir_url(plugin_basename(__FILE__)).'lib/external/' );
 define( 'PWP_VERSION', '1.1' );
 include_once PWP_ROOT . 'lib/native/helpers.php';
-//unset($_SESSION);
+
 spl_autoload_register( 'pwp_class_autoloader' );
-
-
-//}
 
 function pwp_class_autoloader( $classname ) {
     
@@ -48,75 +43,36 @@ function pwp_class_autoloader( $classname ) {
     $module = explode('_', strtolower( $classname ));
     
     array_pop($path);
-//dump($path);
-if(isset($path[0]) && $path[0] == 'interface'){
-     $end = 'interface-'.$end;
-}else{
-    $end = 'class-'.$end;
-}
+    if(isset($path[0]) && $path[0] == 'interface'){
+        $end = 'interface-'.$end;
+    }else{
+        $end = 'class-'.$end;
+    }
    
     $path[] = $end;
-    //dump($path);
     $classname = implode('/',$path);
     $classname = str_replace( '_', '/', strtolower( $classname ) );
     $classfile = sprintf( '%slib/native/%s.php', ABSPATH.'wp-content/plugins/pwp/', str_replace( '_', '-', strtolower( $classname ) ));
 
-
-
     if ( file_exists( $classfile ) ) {
         include_once( $classfile );
     } else {
-        
-        
         $classfile = sprintf( '%smodules/%s/%s.php', ABSPATH.'wp-content/plugins/pwp/', $module[0], 'class-'. implode( '-',$module  ));
-        
-        
-    if ( file_exists( $classfile ) ) {
-        include_once( $classfile );
+        if ( file_exists( $classfile ) ) {
+            include_once( $classfile );
+        }
     }
-    }
-      //dump($classfile);
-      //dump(implode( '-',$module  ));
 }
 
-
-//add_action( 'init', 'pwp_register_autoload' );
-//function pwp_register_autoload(){
-//    spl_autoload_register( 'pwp_class_autoloader' );
-//}
-//function pwp_class_autoloader( $classname ) {
-//    //dump($classname);
-//    $path = explode('_', strtolower( $classname ));
-//    $end = end($path);
-//    array_pop($path);
-//    $end = 'class-'.$end;
-//    $path[] = $end;
-//    $classname = implode('/',$path);
-//    $classname = str_replace( '_', '/', strtolower( $classname ) );
-//    $classfile = sprintf( '%slib/native/%s.php',plugin_dir_path( __FILE__ ), str_replace( '_', '-', strtolower( $classname ) ));
-//    //dump($classfile);
-//    if ( file_exists( $classfile ) ) include_once( $classfile );
-//}
 function load_custom_wp_admin_style() {
         wp_register_style( 'pwp_admin_css', plugins_url( '/lib/pwp-style.css', __FILE__ ), array('theme_style'), PWP_VERSION );
         wp_enqueue_style( 'pwp_admin_css' );
 }
 add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin_style' );
+
 include_once( PWP_ROOT . 'lib/external/meta-box-class/my-meta-box-class.php' );
-
 include_once PWP_ROOT . 'lib/native/class-pwp.php';
-
 include_once PWP_ROOT . 'config/site-config.php';
-
-//require_once PWP_ROOT . 'lib/native/class-email-observer.php';
-//$plugin_instance = Pwp::init( array( '' ) );
-
-//dump($plugin_instance);
-
-
-//$obj->load_class('session');
-
-//$plugin_instance->load_module('cookie-alert');
 
 //register_activation_hook( __FILE__, array( 'Pwp', 'plugin_activation' ) );
 //register_deactivation_hook( __FILE__, array( 'Pwp', 'plugin_deactivation' ) );
