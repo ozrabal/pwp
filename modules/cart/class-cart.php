ef0afcec78f31e1a6e0319bb4edc8a3c15edf227
Piotr Łepkowski <ozrabal@gmail.com>
2014-09-23 15:02:08 +0200

    rozwoj
    dodanie metaboxu z cena, strona koszyka z wirtualnego wywolania

--- modules/cart/class-cart.php
+++ modules/cart/class-cart.php
@@ -26,40 +26,62 @@ class Cart extends Module{
 
     public function __construct() {
 
-
-
 	parent::__construct();
 
 	$this->action_slug = 'cart';
 
-	dump($this->actions);
 	if( !session_id() ) {
 	    Pwp::load_module( 'session' );
 	}
 
-	if(isset($_SESSION['cart'])){
+	if( isset( $_SESSION['cart'] ) ) {
 	    $this->items = $_SESSION['cart'];
 	}
-$this->page =  new Virtualpage();
 
-	$wp_error = $this->route();
-       
+	$this->page = new Virtualpage();
 
-	//$this->cart
+	$this->route();
 
 	wp_enqueue_style( 'dashicons' );
+	add_action( 'init', array( $this, 'add_price_box' ) );
 	add_action( 'the_content', array( $this, 'add_cart_button' ) );
 
-	//add_action( 'get_template_part_content', array( $this, 'add_cart_panel' ) );
-	
-dump(count($this->items));
-
-
 	register_widget( 'Cart_Widget' );
-//$this->update_cart();
+    }
 
-    
 
+    public function add_price_box(){
+
+
+        $box = 
+            array(
+                'name'      => 'Shop',
+                'title'     => __( 'Price', 'pwp' ),
+                //'callback'=> '',
+                'post_type' => array('post'),
+                'elements'  => array(
+
+
+
+
+                    array(
+                        'type' => 'text',
+                        'name' => 'price',
+                        'params'=> array(
+                            'label' => __( 'Cena', 'pwp' ),
+                            'comment' => __('Cena podstawowa','pwp'),
+			    'class'	    => 'small-text block'
+
+
+                        ),
+                    ),
+                )
+            
+        );
+
+            new Metabox( $box );
+
+	
     }
 
 
@@ -71,16 +93,10 @@ dump(count($this->items));
 
     private function add_to_cart( $object){
 
-	//dump($this->items);
+	
 
 	$this->items[] = $object;
-	//dump($this->items);
-
-
-	//dump($_SESSION['cart']);
-
-
-	//$_SESSION['cart'][] = $object;
+	
 
 	
 
@@ -93,7 +109,7 @@ $this->update_cart();
 
 	if ( wp_verify_nonce( $_POST['object_nonce'], 'object_'.$_POST['object_id'] ) ) {
 
-	dump($_POST);
+	//dump($_POST);
 
 	$object = get_post(intval($_POST['object_id']));
 
@@ -111,6 +127,7 @@ $this->update_cart();
     public function add_cart_button( $content ) {
 
 	
+	
 	$object_nonce = wp_create_nonce('object_'.get_the_ID());
 	$content .= '<form action="?cart=add" method="post">';
 	$content .= '<input type="text" name="object_id" value="'.  get_the_ID() . '">';
@@ -123,15 +140,12 @@ $this->update_cart();
 
     public function cart_panel () {
 	
-        //dump($_SESSION);
-	dump(count($_SESSION['cart']));
-
-        dump(count($this->items));
+    
 
 	
 
 
-	echo '<a href="'.esc_url( home_url( '/cart/?cart=show' ) ).'">zobacz koszyk</a>';
+	echo '<a href="'.esc_url( home_url( '/cart' ) ).'">zobacz koszyk</a>';
 
 
 
@@ -140,12 +154,15 @@ $this->update_cart();
 
     public function index_Action(){
 
-        dump($this->page);
+        dump(__METHOD__);
+        //$this->page->add('#(cart)|(\/[?])(.[a-z]*)[=](.[a-z]*)#', array( $this, 'mytest_contentfunc'));
+
+   //$this->page->add('/(cart)(\\/[?])(.[a-z]*)[=](.[a-z]*)/i', array( $this, 'mytest_contentfunc'));
+        
         $this->page->add('/cart/', array( $this, 'mytest_contentfunc'));
 
-  
-        
-        
+
+
     }
 
 
@@ -169,6 +186,11 @@ $this->update_cart();
 
 	//dump($v);
 
+    }
+
+
+    public function pay_Action(){
+	dump(__METHOD__);
     }
 
   public function show_title($item){
