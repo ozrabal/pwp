dba269e9a03f1ab6be7ae362a720edcef5c2f4f8
Piotr Łepkowski <ozrabal@gmail.com>
2014-10-09 15:10:18 +0200

    pole typu file
    z uploadem obsluzonym przez wp_handle_upload

--- lib/native/formelement/class-file.php
+++ lib/native/formelement/class-file.php
@@ -1,67 +1,28 @@
 <?php
-class Formelement_File extends Formelement{
+
+class Formelement_File extends Formelement {
+
     protected $type = 'file';
 
+    public function __construct( $form, $name ) {
 
-
-    public function __construct($form, $name){
-	dump($_FILES);
-   dump($_POST);
-   dump($form->get_name());
-   //die();
-	if(isset($_FILES[$form->get_name()])){
-
-	$this->validate_setting($form->get_name());
-	}
-	parent::__construct($form, $name);
-
-
-	//die();
+	parent::__construct( $form, $name );
     }
 
-public function validate_setting($s) {
-    $keys = array_keys($_FILES);
-    $i = 0;
-    require_once ( ABSPATH . 'wp-admin/includes/file.php' );
-    foreach ( $_FILES as $image ) {
-$im = array();
-	foreach($image as $param => $val){
-	    dump($val);
-	   $im[$param] = $val['plik'];
+    public function on_submit() {
+	if( isset( $_FILES[$this->get_name()] ) && $_FILES[$this->get_name()]['name'] != '' ) {
+	    require_once ( ABSPATH . 'wp-admin/includes/file.php' );
+	    $upload_overrides = array( 'test_form' => false );
+	    $file = wp_handle_upload( $_FILES[$this->get_name()] ,$upload_overrides);
+	    if( isset( $file['file'] ) ) {
+		$_POST[$this->get_name()] = $file;
+		$this->form->set_request($_POST);
+	    }
 	}
-
-	// if a files was upload
-	dump($im);
-	//if ($image['size']) {
-
-$upload_overrides = array( 'test_form' => false );
- 
-    // save the file, and store an array, containing its location in $file
-    $file = wp_handle_upload( $im ,$upload_overrides);
-
-   dump($file);
-
-	die();
-	//}
-
-	}
-
-}
-
-	
-    
-
-
-
-    public function render(){
-
-        
-             return  $this->get_before().$this->get_label().'<input '.$this->id().$this->type().$this->name().$this->value().$this->cssclass().'/>'.$this->get_message().$this->get_comment('<p class="description">%s</p>').$this->get_after();
-
-        
-	//return '<input type="file" name="img">';
-	
     }
 
-    
+    public function render() {
+
+	return  $this->get_before() . $this->get_label() . '<input ' . $this->id() . $this->type() . $this->name() . $this->value() . $this->cssclass() . '/>' . $this->get_message() . $this->get_comment( '<p class="description">%s</p>' ) . $this->get_after();
+    }
 }
