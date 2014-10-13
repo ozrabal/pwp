f51aee5f5190130d99f3f248d6db592aecb92907
Piotr Łepkowski <ozrabal@gmail.com>
2014-10-13 23:06:02 +0200

    pole mapa
    rezynacja z automatycznej pozycji przez niepelna obsluge w firefoxie

--- lib/native/formelement/class-map.php
+++ lib/native/formelement/class-map.php
@@ -8,20 +8,32 @@
    */
 class Formelement_Map extends Formelement_Input {
     protected $type = 'map';
-
+    
+    /**
+     * konstruktor
+     * @param Form $form
+     * @param string $name
+     */
     public function __construct( $form, $name ) {
 	
         add_action( 'admin_init', array( $this, 'admin_enqueue_scripts' ) );
 	parent::__construct( $form, $name );
     }
-
+    
+    /**
+     * dolaczenie skryptow
+     */
     function admin_enqueue_scripts() {
 
 	wp_enqueue_script( 'maps', 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places&sensor=false' );
-	wp_enqueue_script( 'field-map', plugins_url( '/field-maps.js', __FILE__ ), array( 'jquery' ), PWP_VERSION );
+	wp_enqueue_script( 'field-map', plugins_url( '/field-map.js', __FILE__ ), array( 'jquery' ), PWP_VERSION );
 	wp_localize_script( 'field-map', 'geocode_notfound', __( 'No results were found for the search criteria', 'pwp' ) );
     }
-
+    
+    /**
+     * renderuje pole
+     * @return string
+     */
     public function render(){
 
 	parent::render();
@@ -29,6 +41,6 @@ class Formelement_Map extends Formelement_Input {
 	if( WP_DEBUG ) {
 	    $type = 'text';
 	}
-        return  $this->get_before().$this->get_label().'<div class="box"><input onkeydown="if (event.keyCode == 13){ codeAddress(); return false;}" id="pac-input" class="controls" type="text" placeholder="' . __( 'Type location', 'pwp' ) . '"><input type="button" class="button button-small" value="' . __( 'Show on map', 'pwp' ) . '" onclick="codeAddress();return false;"></div><div id="map"></div><input '.$this->id().' type="'.$type.'" '.$this->name().$this->value().$this->cssclass().'/>'.$this->get_message().$this->get_comment('<p class="description">%s</p>').$this->get_after();
+        return  $this->get_before() . $this->get_label() . '<div class="box"><input onkeydown="if (event.keyCode == 13){ codeAddress(); return false;}" id="geocode" class="controls" type="text" placeholder="' . __( 'Type location', 'pwp' ) . '"><input type="button" class="button button-small" value="' . __( 'Show on map', 'pwp' ) . '" onclick="codeAddress();return false;"></div><div id="map"></div><input ' . $this->id() . ' type="' . $type . '" ' . $this->name() . $this->value() . $this->cssclass() . '/>' . $this->get_message() . $this->get_comment( '<p class="description">%s</p>' ) . $this->get_after();
 	}
 }
