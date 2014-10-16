2b3bbc3cad793bfbfd280d7cbac5cd738b861419
Piotr Łepkowski <ozrabal@gmail.com>
2014-10-16 23:00:28 +0200

    option w select
    bugfix, nie ustawial value

--- lib/native/formelement/class-select.php
+++ lib/native/formelement/class-select.php
@@ -18,6 +18,7 @@ class Formelement_Select extends Formelement {
 
         foreach( $options as $name => $value ) {
 	    $option = new Formelement_Option( $this->form, $name, $this );
+            $option->set_value($value);
 	    if( $this->get_value() == $value ) {
                 $option->selected = true;
             }
